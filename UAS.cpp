9367492faea0325bdbf7e13d633ca93933a88126
Frankiee77 <georgejos79z@gmail.com>
2023-01-12 19:26:17 +0700

    new file:   UAS.cpp 	new file:   UAS.exe

---
+++ UAS.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string nama, prodi, fakultas;
+    int nim;
+        cout << "Nama\t: ";
+        getline (cin,nama);
+        cout << "NIM\t: ";
+        cin >> nim;
+        cout << "Prodi\t: ";
+        getline (cin,prodi);
+        cout << "Fakultas: ";
+        cin >> fakultas;
+    
+    
+return 0;
+}
