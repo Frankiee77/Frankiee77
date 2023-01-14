0758f901bb0d502d3a23fca3c476bf15c0957be5
Frankiee77 <georgejos79z@gmail.com>
2023-01-14 20:26:42 +0700

    2nd

--- UAS.cpp
+++ UAS.cpp
@@ -2,13 +2,45 @@
 using namespace std;
 
 int main(){
+    char ulang;
+    cout << "PROGRAM DATA MAHASISWA\n";
+    
+    do{
+    int pilih, nim;
     string nama, prodi, fakultas;
-    int nim;
-        cout << "Nama\t: ";
-        getline (cin,nama);
-        cout << "NIM\t: ";
-        cin >> nim;
-        cout << "Fakultas: ";
-        cin >> fakultas;
+    
+
+        cout << "Pilihan Menu\n";
+        cout << "1. Input data mahasiswa\n";
+        cout << "2. Lihat data mahasiswa\n";
+        cout << "3. Ubah data mahasiswa\n\n";
+        cout << "Pilih menu : ";
+        cin >> pilih;
+
+        switch (pilih){
+        case 1:
+            system("CLS");
+            cout << "Nama\t :";
+            getline(cin, nama);
+            cout << nama;
+            break;
+        
+        case 2:
+            
+            break;
+        }
+
+    cout << endl;
+    cout << "Kembali ke menu awal? (y/n): ";
+    cin >> ulang;
+    system("CLS");
+
+    }while (ulang == 'y' | ulang == 'Y');
+     cout << endl;
+     cout << "Program telah selesai!";
+     cout << endl;
+
+return 0;
+    
     
 }
