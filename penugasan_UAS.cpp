f86f1779b5db5a074fd097b9076e6ebf8a8ee4d8
Frankiee77 <georgejos79z@gmail.com>
2023-01-28 12:53:39 +0700

    final

--- penugasan_UAS.cpp
+++ penugasan_UAS.cpp
@@ -26,13 +26,13 @@ void pilih(){
     cout << "Pilihan Anda : ";
     cin >> pilihan;
     if (pilihan==1){
-        short clss[3] = {1, 2, 3};
-        string kls[3] = {". First class", ". Bussiness class", ". Economy class"};
+        short kelasA[3] = {1, 2, 3};
+        string kelasB[3] = {". First class", ". Bussiness class", ". Economy class"};
         string dari, ke;
         short kelas;
         unsigned short tgl1, tgl3;
         string tgl2;
-        short pnpg1, pnpg2, pnpg3;
+        short penumpang1, penumpang2, penumpang3;
         system ("cls");
         cout << "<><><><><><><><><><><><><>" << endl;
         cout << "<>  MENU TIKET PESAWAT  <>" << endl;
@@ -48,59 +48,27 @@ void pilih(){
             cout << endl;
             for (int i = 0; i < 3; i++)
             {
-                cout << clss[i] << kls[i] << endl;
+                cout << kelasA[i] << kelasB[i] << endl;
             }
             cout << "Pilih kelas mana yang akan dipilih : ";
             cin >> kelas; cout << endl;
         cout << "Penumpang" << endl;
-            cout << "Dewasa \t: "; cin >> pnpg1;
-            cout << "Anak \t: "; cin >> pnpg2;
-            cout << "Balita \t: "; cin >> pnpg3;
+            cout << "Dewasa \t: "; cin >> penumpang1;
+            cout << "Anak \t: "; cin >> penumpang2;
+            cout << "Balita \t: "; cin >> penumpang3;
     }
     else if(pilihan==2){
-        short clss[3] = {1, 2, 3};
-        string kls[3] = {". First class", ". Bussiness class", ". Economy class"};
-        string dari, ke;
-        short kelas;
-        unsigned short tgl1, tgl3;
-        short tgl2;
-        short pnpg1, pnpg2, pnpg3;
-        system ("cls");
-        cout << "<><><><><><><><><><><><><>" << endl;
-        cout << "<>  MENU TIKET KERETA  <>" << endl;
-        cout << "<><><><><><><><><><><><><>" << endl << endl;
-        cout << "Tujuan" << endl;
-            cout << "Perjalanan dari : "; 
-            cin >> dari;
-            cout << "Menuju ke \t: ";
-            cin >> ke; cout << "\n";
-        cout << "Keberangkatan" << endl;
-            cout << "Tentukan tanggalnya : ";
-            cin >> tgl1; cout <<" "; cin >> tgl2; cout << " "; cin >> tgl3;
-            cout << endl;
-            for (int i = 0; i < 3; i++)
-            {
-                cout << clss[i] << kls[i] << endl;
-            }
-            cout << "Pilih kelas mana yang akan dipilih : ";
-            cin >> kelas; cout << endl;
-        cout << "Penumpang" << endl;
-            cout << "Dewasa \t: "; cin >> pnpg1;
-            cout << "Anak \t: "; cin >> pnpg2;
-            cout << "Balita \t: "; cin >> pnpg3;
-    }
-    else if(pilihan==3){
-       short clss[2] = {1, 2};
-        string kls[2] = {". Express class", ". Reguler class"};
+        short kelasA[2] = {1, 2};
+        string kelasB[2] = {". Express class", ". Reguler class"};
         string dari, ke;
         short kelas;
         unsigned short tgl1, tgl3;
         string tgl2;
-        short pnpg1, pnpg2, pnpg3;
+        short penumpang1, penumpang2, penumpang3;
         system ("cls");
-        cout << "<><><><><><><><><><><><><>" << endl;
-        cout << "<>  MENU TIKET KAPAL LAUT  <>" << endl;
-        cout << "<><><><><><><><><><><><><>" << endl << endl;
+        cout << "<><><><><><><><><><><><><><><>" << endl;
+        cout << "<>  MENU TIKET KAPAL LAUT   <>" << endl;
+        cout << "<><><><><><><><><><><><><><><>" << endl << endl;
         cout << "Tujuan" << endl;
             cout << "Perjalanan dari : "; 
             cin >> dari;
@@ -112,14 +80,46 @@ void pilih(){
             cout << endl;
             for (int i = 0; i < 2; i++)
             {
-                cout << clss[i] << kls[i] << endl;
+                cout << kelasA[i] << kelasB[i] << endl;
             }
             cout << "Pilih kelas mana yang akan dipilih : ";
             cin >> kelas; cout << endl;
         cout << "Penumpang" << endl;
-            cout << "Dewasa \t: "; cin >> pnpg1;
-            cout << "Anak \t: "; cin >> pnpg2;
-            cout << "Balita \t: "; cin >> pnpg3;
+            cout << "Dewasa \t: "; cin >> penumpang1;
+            cout << "Anak \t: "; cin >> penumpang2;
+            cout << "Balita \t: "; cin >> penumpang3;
+    }
+    else if(pilihan==3){
+        short kelasA[3] = {1, 2, 3};
+        string kelasB[3] = {". First class", ". Bussiness class", ". Economy class"};
+        string dari, ke;
+        short kelas;
+        unsigned short tgl1, tgl3;
+        string tgl2;
+        short penumpang1, penumpang2, penumpang3;
+        system ("cls");
+        cout << "<><><><><><><><><><><><><><><><>" << endl;
+        cout << "<>    MENU TIKET KERETA API   <>" << endl;
+        cout << "<><><><><><><><><><><><><><><><>" << endl << endl;
+        cout << "Tujuan" << endl;
+            cout << "Perjalanan dari : "; 
+            cin >> dari;
+            cout << "Menuju ke \t: ";
+            cin >> ke; cout << "\n";
+        cout << "Keberangkatan" << endl;
+            cout << "Tentukan tanggalnya : ";
+            cin >> tgl1; cout <<" "; cin >> tgl2; cout << " "; cin >> tgl3;
+            cout << endl;
+            for (int i = 0; i < 3; i++)
+            {
+                cout << kelasA[i] << kelasB[i] << endl;
+            }
+            cout << "Pilih kelas mana yang akan dipilih : ";
+            cin >> kelas; cout << endl;
+        cout << "Penumpang" << endl;
+            cout << "Dewasa \t: "; cin >> penumpang1;
+            cout << "Anak \t: "; cin >> penumpang2;
+            cout << "Balita \t: "; cin >> penumpang3;
     }
     else{
         cout << "Mohon maaf, pilihan tidak tersedia." << endl << endl;
@@ -135,23 +135,30 @@ struct datadiri{
     string nama;
     unsigned long NIK;
     int hp;
-}data1, data2;
+}data1;
 
 int main(){
-    
-    menu();
-    pilih();
-    cout << endl;
-    cout << "Data diri" << endl;
-        cout << "Nama \t : ";
-        cin.ignore (1, '\n');
-        getline (cin, data1.nama);
-        cout << "NIK \t : ";
-        cin >> data1.NIK;
-        cout << "Nomor Hp : "; 
-        cin >> data1.hp;
-    cout << endl;
-    akhir("Terimakasih telah memesan di aplikasi kami.");
+    char ulang;
+        do{
+        menu();
+        pilih();
+        cout << endl;
+        cout << "Data diri" << endl;
+            cout << "Nama \t : ";
+            cin.ignore();
+            getline (cin, data1.nama);
+            cout << "NIK \t : ";
+            cin >> data1.NIK;
+            cout << "Nomor Hp : "; 
+            cin >> data1.hp;
+        cout << endl;
+        akhir("Terimakasih telah memesan di aplikasi kami.");
+
+        cout << endl;
+        cout << "Kembali ke menu awal? (y/n): ";
+        cin >> ulang;
+        system("CLS");
+        }while (ulang == 'Y' || ulang == 'y');
     
     
     return 0; 
